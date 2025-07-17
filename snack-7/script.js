6a0bbb742e1544a57881cf1100253074f76a9937
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-17 14:56:52 +0200

    mnfix: small fixes for better code reading

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,7 +21,5 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
-const student = students.find((students) => {
-  return students.id === 2;
-})
+const student = students.find((students) => students.id === 2)
 console.log(student)
