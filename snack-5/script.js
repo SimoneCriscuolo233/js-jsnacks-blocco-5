40d1c23c641534ade0e1d5b49fdbcdabef4b2fcf
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-17 14:17:32 +0200

    feat: Create an array from the nums array, containing only even numbers.

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,10 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+const even = [];
+nums.filter((nums) => {
+  if (nums % 2 === 0) {
+    even.push(nums)
+  }
+})
+console.log(even)
